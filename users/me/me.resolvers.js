811300e38db6a72688e161a388d367327dc867cc
sumin <lsm001005@naver.com>
2022-05-12 02:09:03 +0900

    Modify me

--- users/me/me.resolvers.js
+++ users/me/me.resolvers.js
@@ -3,12 +3,13 @@ import jwt from "jsonwebtoken";
 
 export default {
     Query: {
-        me: async (token) => {
-            if (!token) {
-                return null;
-            }
+        me: async (_, { token }) => {
             const { id } = await jwt.verify(token, process.env.SECRET_KEY);
-            client.user.findUnique({ where: { id } });
+            return client.user.findUnique({
+                where: {
+                    id
+                },
+            })
         }
     },
 };
