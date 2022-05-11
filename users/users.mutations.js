36910ceb501ae0645454b7a59ffa7246a9f6a408
sumin <lsm001005@naver.com>
2022-05-11 16:01:13 +0900

    Print an error message if an email exists

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -9,18 +9,25 @@ export default {
             company,
             password,
         }) => {
-            const existingUser = await client.user.findFirst({
-                where: { email }
-            });
-            const uglyPassword = await bcrypt.hash(password, 10);
-            return client.user.create({
-                data: {
-                    name,
-                    email,
-                    company,
-                    password: uglyPassword,
-                },
-            });
+            try {
+                const existingUser = await client.user.findFirst({
+                    where: { email }
+                });
+                if (existingUser) {
+                    throw new Error("This email is already taken.");
+                }
+                const uglyPassword = await bcrypt.hash(password, 10);
+                return client.user.create({
+                    data: {
+                        name,
+                        email,
+                        company,
+                        password: uglyPassword,
+                    },
+                });
+            } catch (e) {
+                return e;
+            }
 
         },
     }
