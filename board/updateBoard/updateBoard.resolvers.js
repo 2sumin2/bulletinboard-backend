72ab648168c7fc4444d893e4e90471502610fdaf
sumin <lsm001005@naver.com>
2022-05-14 05:56:18 +0900

    UpdateBoard

--- board/updateBoard/updateBoard.resolvers.js
+++ board/updateBoard/updateBoard.resolvers.js
@@ -18,13 +18,11 @@ export default {
             try {
                 let attachedFileUrl = null;
                 if (attachedFile) {
-
                     const { filename, createReadStream } = await attachedFile;
                     const readsStream = createReadStream();
                     const newFilename = `${authorId}-${Date.now()}-${filename}`;
                     const writeStream = fs.createWriteStream(process.cwd() + "/uploads/" + newFilename);
                     readsStream.pipe(writeStream);
-
                     attachedFileUrl = `http://localhost:4000/static/${newFilename}`;
                 }
                 await client.board.update({
@@ -47,7 +45,7 @@ export default {
             } catch (e) {
                 return {
                     ok: false,
-                    error: e,
+                    error: "게시글을 삭제할 수 없습니다.",
                 };
             }
         },
