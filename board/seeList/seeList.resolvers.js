4ce6486500f5e5ef29f69ab0e0dc5c7448d3a628
sumin <lsm001005@naver.com>
2022-05-14 03:07:56 +0900

    Ordery by id

--- board/seeList/seeList.resolvers.js
+++ board/seeList/seeList.resolvers.js
@@ -3,7 +3,11 @@ import client from "../../client";
 export default {
     Query: {
         seeList: async (_,) => {
-            return client.board.findMany()
+            return client.board.findMany({
+                orderBy: {
+                    id: 'asc',
+                },
+            })
         }
     },
 };
