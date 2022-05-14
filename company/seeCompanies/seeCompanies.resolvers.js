36f5aed443cca32fa346e289671120aeffff997f
sumin <lsm001005@naver.com>
2022-05-14 19:11:04 +0900

    See the list of companies

---
+++ company/seeCompanies/seeCompanies.resolvers.js
@@ -0,0 +1,13 @@
+import client from "../../client";
+
+export default {
+    Query: {
+        seeCompanies: async (_,) => {
+            return client.company.findMany({
+                orderBy: {
+                    id: 'desc',
+                },
+            })
+        }
+    },
+};
